1cf4e6f807480db9320c5c0dd944de57d07e8c1f
dcbryant <dcbryant@753gmail.com>
2018-07-10 18:40:00 +0800

    init && config less

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, {Component, Fragment} from 'react';
+import { Button } from './components';
+
+class App extends Component {
+
+    render(){
+        return (
+            <Fragment>
+                <Button />
+            </Fragment>
+        )
+    }
+}
+
+export default App;
---
+++ src/components/button/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import styles from './index.less'
+
+class Button extends Component {
+
+    render(){
+        return (
+            <div className={styles.button}>
+                button
+            </div>
+        )
+    }
+}
+export default Button;
